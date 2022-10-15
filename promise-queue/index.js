844d51fe13061e796134a10b6ee6c5497cb80daf
bob <bob@bobdeMacBook-Air.local>
2022-10-15 15:03:15 +0800

    step1 finished

--- promise-queue/index.js
+++ promise-queue/index.js
@@ -1,15 +1,21 @@
 
 function promiseQueue(promiseArr, concurrentNum){
     
-    let result = [];
     //排队中的promise waiting[]
+    let waiting = [];
     //运行中的promise running[]
+    let running = [];
     //完成的promises的values finished[]
+    let finished = [];
     //1.从promiseArr里拿 Math.min(concurrentNum, promiseArr.length) 个promise, 放进running[]
+    for(var i=0; i<Math.min(concurrentNum, promiseArr.length); i++){
+        const promise = promiseArr[i]
+        waiting.push(promise)
+    }
     //2.这些promise如果有结果，就放进finished[], 再从waiting里面取出一个，放进running
     //3.循环2，直到finished跟promiseArr长度一致，放回finished
 
-    return result;
+    return finished;
 }
 
 //test
