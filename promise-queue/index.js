b892d43eec36b73898497f577ae4ebcd89f0fe24
bob <bob@bobdeMacBook-Air.local>
2022-10-15 14:56:38 +0800

    逻辑思路完成，下一步翻译代码

--- promise-queue/index.js
+++ promise-queue/index.js
@@ -1,8 +1,14 @@
 
 function promiseQueue(promiseArr, concurrentNum){
-    //return allPromiseResults
+    
     let result = [];
-    result = Promise.allSettled(promiseArr)
+    //排队中的promise waiting[]
+    //运行中的promise running[]
+    //完成的promises的values finished[]
+    //1.从promiseArr里拿 Math.min(concurrentNum, promiseArr.length) 个promise, 放进running[]
+    //2.这些promise如果有结果，就放进finished[], 再从waiting里面取出一个，放进running
+    //3.循环2，直到finished跟promiseArr长度一致，放回finished
+
     return result;
 }
 
