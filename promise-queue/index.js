25374c367d7a17ca70cd00e73f0f750b8850ab12
bob <bob@bobdeMacBook-Air.local>
2022-10-15 15:37:34 +0800

    函数抽出去了，而且还递归了。利用边界做了退出判断。下面处理函数返回值的同步问题

--- promise-queue/index.js
+++ promise-queue/index.js
@@ -1,4 +1,5 @@
 
+
 function promiseQueue(promiseArr, concurrentNum){
     
     //排队中的promise waiting[]
@@ -10,31 +11,29 @@ function promiseQueue(promiseArr, concurrentNum){
     let finished = [];
     //1.从promiseArr里拿 Math.min(concurrentNum, promiseArr.length) 个promise, 放进running[]
     //2.这些promise如果有结果，就放进finished[], 再从waiting里面取出一个，放进running
-    for(; i<Math.min(concurrentNum, promiseArr.length); i++){
-        const promise = promiseArr[i]
-        .then(v => {
-            const [idx, result] = v
-            finished[idx] = result;
-            //running 删除对应位置的promise， TODO： 对应位置怎么获取
-            //running 的位置没有固定规则，考虑到running在初始化是最多concurrentNum个
-            //后面是完成一个，进一个。一个萝卜一个坑，所以，running的长度不重要了
-            //再放置一个进去 TODO：下一个位置怎么获取
-            //从全局i可以获取下一个promise
-            running.push(promiseArr[i++])
-        })
-        .catch(v => {
-            const [idx, result] = v
-            finished[idx] = result; 
-            //running 删除对应位置的promise， TODO： 对应位置怎么获取
-            //running 的位置没有固定规则，考虑到running在初始化是最多concurrentNum个
-            //后面是完成一个，进一个。一个萝卜一个坑，所以，running的长度不重要了
-            //再放置一个进去 TODO：下一个位置怎么获取
-            //从全局i可以获取下一个promise
-            running.push(promiseArr[i++])
-        })
+    for(; i<Math.min(concurrentNum, promiseArr.length); ){
+        const promise = promiseArr[i++]
+        .then(promiseSettled)
+        .catch(promiseSettled)
         running.push(promise)
     }
     //3.循环2，直到finished跟promiseArr长度一致，放回finished
+
+
+    function promiseSettled(v) {
+        if(i >= promiseArr.length) return;
+        const [idx, result] = v
+        finished[idx] = result;
+        //running 删除对应位置的promise， TODO： 对应位置怎么获取
+        //running 的位置没有固定规则，考虑到running在初始化是最多concurrentNum个
+        //后面是完成一个，进一个。一个萝卜一个坑，所以，running的长度不重要了
+        //再放置一个进去 TODO：下一个位置怎么获取
+        //从全局i可以获取下一个promise
+        const promise = promiseArr[i++]
+            .then(promiseSettled)
+            .catch(promiseSettled)
+        running.push(promise)
+    }
 
     return finished;
 }
