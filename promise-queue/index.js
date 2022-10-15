c61cd5f76c956dc7be420655b5ea5c888bf6396d
bob <bob@bobdeMacBook-Air.local>
2022-10-15 14:44:33 +0800

    全部生成

--- promise-queue/index.js
+++ promise-queue/index.js
@@ -1,4 +1,21 @@
 
 function promiseQueue(promiseArr, concurrentNum){
     //return allPromiseResults
+    let result = [];
+    result = Promise.allSettled(promiseArr)
+    return result;
 }
+
+//test
+function genPromise(n){
+    return new Promise((res, rej) => {
+        setTimeout(res, n*100, n);
+    })
+}
+var promiseArr = [
+    genPromise(2),
+    genPromise(1),
+    genPromise(3)
+]
+let t = promiseQueue(promiseArr)
+console.dir(t)
