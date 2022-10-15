edb9ef88d6ba0712fb41121e46277aa6f8b0f838
bob <bob@bobdeMacBook-Air.local>
2022-10-15 15:29:02 +0800

    基本完成了框架逻辑，下面处理边界。什么时候停止放入

--- promise-queue/index.js
+++ promise-queue/index.js
@@ -2,14 +2,15 @@
 function promiseQueue(promiseArr, concurrentNum){
     
     //排队中的promise waiting[]
-    let waiting = [];
+    //也不用waiting了，直接用一个全局i来在promiseArr中遍历游走即可
+    let i = 0;
     //运行中的promise running[]
     let running = [];
     //完成的promises的values finished[]
     let finished = [];
     //1.从promiseArr里拿 Math.min(concurrentNum, promiseArr.length) 个promise, 放进running[]
     //2.这些promise如果有结果，就放进finished[], 再从waiting里面取出一个，放进running
-    for(var i=0; i<Math.min(concurrentNum, promiseArr.length); i++){
+    for(; i<Math.min(concurrentNum, promiseArr.length); i++){
         const promise = promiseArr[i]
         .then(v => {
             const [idx, result] = v
@@ -18,6 +19,8 @@ function promiseQueue(promiseArr, concurrentNum){
             //running 的位置没有固定规则，考虑到running在初始化是最多concurrentNum个
             //后面是完成一个，进一个。一个萝卜一个坑，所以，running的长度不重要了
             //再放置一个进去 TODO：下一个位置怎么获取
+            //从全局i可以获取下一个promise
+            running.push(promiseArr[i++])
         })
         .catch(v => {
             const [idx, result] = v
@@ -26,6 +29,8 @@ function promiseQueue(promiseArr, concurrentNum){
             //running 的位置没有固定规则，考虑到running在初始化是最多concurrentNum个
             //后面是完成一个，进一个。一个萝卜一个坑，所以，running的长度不重要了
             //再放置一个进去 TODO：下一个位置怎么获取
+            //从全局i可以获取下一个promise
+            running.push(promiseArr[i++])
         })
         running.push(promise)
     }
